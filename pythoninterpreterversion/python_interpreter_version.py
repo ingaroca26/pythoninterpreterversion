c38fff9323e17dbce3d4a22a7060a3a63380098c
ingaroca26 <ingaroca26@outlook.com>
2022-02-20 12:28:05 -0500

    v0.0.6

---
+++ pythoninterpreterversion/python_interpreter_version.py
@@ -0,0 +1,6 @@
+import sys
+
+
+python_interpreter_version: str = sys.version
+f_python_interpreter_version: str = f'Python interpreter version is: {python_interpreter_version}'
+print(f_python_interpreter_version)
